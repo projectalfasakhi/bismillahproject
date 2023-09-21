91ca168a8fb8d81d5112a2836d777bb51c4ee5e7
Your Name <nabilgelar344@gmail.com>
2023-09-21 16:10:11 +0700

    adminlte

---
+++ resources/views/layout/loader.blade.php
@@ -0,0 +1,12 @@
+<div class="content-wrapper iframe-mode" data-widget="iframe" data-loading-screen="750">
+    <div class="tab-content">
+        <div class="tab-empty">
+          <h2 class="display-4">No tab selected!</h2>
+        </div>
+        <div class="tab-loading">
+          <div>
+            <h2 class="display-4">Tab is loading <i class="fa fa-sync fa-spin"></i></h2>
+          </div>
+        </div>
+      </div>
+</div>
